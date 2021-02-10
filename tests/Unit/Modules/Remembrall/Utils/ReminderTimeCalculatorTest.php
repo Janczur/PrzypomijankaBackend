5435183c5fae64aad86cdcc82915a9440e821ac7
Janczur <j.przybysz@grupatense.pl>
2021-02-10 21:53:16 +0100

    Adding reminder time calculator tests

---
+++ tests/Unit/Modules/Remembrall/Utils/ReminderTimeCalculatorTest.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\Tests\Unit\Modules\Remembrall\Utils;
+
+
+use App\DataFixtures\Modules\Remembrall\Entity\ReminderFixtures;
+use App\Modules\Remembrall\Entity\Cyclic;
+use App\Modules\Remembrall\Entity\CyclicType;
+use App\Modules\Remembrall\Entity\Reminder;
+use App\Modules\Remembrall\Utils\ReminderTimeCalculator;
+use DateInterval;
+use DateTime;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ReminderTimeCalculatorTest extends KernelTestCase
+{
+    use FixturesTrait;
+
+    /** @test */
+    public function milliseconds_from_now_until_given_date_time_are_calculated_correctly(): void
+    {
+        $now = (new DateTime())->getTimestamp();
+        $to = new DateTime('+2 day');
+        $expectedMilliseconds = ($to->getTimestamp() - $now) * 1000;
+        $reminderCalculator = new ReminderTimeCalculator();
+        $actualMilliseconds = $reminderCalculator->getRemainingMillisecondsUntil($to);
+        self::assertEquals($expectedMilliseconds, $actualMilliseconds);
+    }
+
+    /** @test  */
+    public function it_calculates_date_of_next_remind_correctly(): void
+    {
+        /** @var Reminder $reminder */
+        $reminder = $this->loadFixtures([ReminderFixtures::class])
+            ->getReferenceRepository()
+            ->getReference(ReminderFixtures::getReferenceKey(0));
+        $cyclic = (new Cyclic())
+            ->setPeriodicity(3)
+            ->setType((new CyclicType())->setName('Month'));
+        $reminder->setCyclic($cyclic);
+        $expectedNextRemindDate = $reminder->getRemindAt()->add(new DateInterval('P3M'));
+        $reminderCalculator = new ReminderTimeCalculator();
+        $reminderCalculator->calculateNextReminderDate($reminder);
+        self::assertEquals($expectedNextRemindDate, $reminder->getRemindAt());
+    }
+}
